8ecd1630397c8ec2bbbbd5f0d37497d39a0f699f
dodevs <douglas.sousa@aevo.com.br>
2023-04-14 00:40:02 -0300

    fix(cli): generate correct script

--- packages/cli/src/lib/generate/templates.ts
+++ packages/cli/src/lib/generate/templates.ts
@@ -2,7 +2,8 @@ export const TypesList = ['script', 'clients'] as const;
 export type Types = typeof TypesList[number];
 
 const ScriptTemplate = `
-exports.default = async function (request, client) {
+exports.default = async function (transaction, client) {
+  const request = new Request(transaction);
   const query = await request.query(\`
     SELECT
       'Hello World' AS [Hello World]
