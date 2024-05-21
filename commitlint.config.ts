391ac423169608acea994b80187874c27198c68e
Douglas Silva <douglass.sousa@outlook.com.br>
2024-05-21 18:37:13 -0300

    chore: change commitlint file and set ignore funcs

---
+++ commitlint.config.ts
@@ -0,0 +1,10 @@
+const Configuration = {
+  extends: ["@commitlint/config-conventional"],
+  ignores: [
+    (message) => /^Bumps \[.+]\(.+\) from .+ to .+\\.$/m.test(message),
+    (message) => /\#\#\s?\[\d+\.\d+\.\d+\]/m.test(message),
+  ],
+  rules: {}
+}
+
+export default Configuration
