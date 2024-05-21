f21d8f4caae918c64a5a4e7f24993777ec61b610
Douglas Silva <douglass.sousa@outlook.com.br>
2024-05-21 18:41:06 -0300

    chore: ingore old semantic release commits

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,7 +2,7 @@ const Configuration = {
   extends: ["@commitlint/config-conventional"],
   ignores: [
     (message) => /^Bumps \[.+]\(.+\) from .+ to .+\\.$/m.test(message),
-    (message) => /\#\#\s?\[\d+\.\d+\.\d+\]/m.test(message),
+    (message) => /\#?\#\s?\[\d+\.\d+\.\d+\]/m.test(message),
   ],
   rules: {}
 }
